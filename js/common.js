64243a52c34a8f3be81cdc152c9fda0da9a5783b
kwon <kwon@kwonui-MacBook.local>
2018-01-02 00:59:33 +0900

    pagenation기능 구현현, 버튼의 중복클릭 방지

--- js/common.js
+++ js/common.js
@@ -1,61 +1,79 @@
-function carousel(car){
+function carousel(car) {
     let carousel_box = car.target;
     let carousel_Item = Array.from(carousel_box.children[0].children);
     let current = 0;
-    let pre_img = carousel_Item.length-1;
-    let next_img = current+1;
+    let pre_img = carousel_Item.length - 1;
+    let next_img = current + 1;
+    let btns = classFind(carousel_box, "btn");
     let next_btn = carousel_box.children[1].children[0];
     let pre_btn = carousel_box.children[1].children[1];
-    
-    next_btn.addEventListener("click", ()=>{
-        current+=1;
-        nextState(true);
-    })
-    pre_btn.addEventListener("click", ()=>{
-        current-=1;
-        nextState(false);
-    })
-    
-    function carouselInit(){
+    //전달한 element 자식요소중 요청한 class를 가진 자식요소를 전부 찾아서 반환해주는 함수
+    function classFind(el, className) {
+        let arr = [];
+        function find(el, className) {
+            if (el.children.length > 0) {
+                for (let item of Array.from(el.children)) {
+                    let isClass = find(item, className);
+                    if (isClass != null) {
+                        arr.push(isClass);
+                    } 
+                }
+            } else {
+                return el.classList.contains(className) ? el : null;
+            }
+        }
+        find(el, className);
+        return arr;
+    };
+
+//클릭이벤트의 중복을 방지하기 위하여 한번 클릭 시 클릭이벤트를 지우고 0.6초 후에 다시 이벤트를 등록해주는 함수
+    function clickBtn(){
+        if(this.classList.contains("next-btn")){
+            current += 1;
+            nextState(true);
+        } else if(this.classList.contains("pre-btn")){
+            current -= 1;
+            nextState(false);
+        }
+        this.removeEventListener("click", clickBtn);
+        setTimeout(()=>{
+            this.addEventListener("click", clickBtn)
+        }, 600);
+    };
+//버튼요소를 순회하면서 각각 이벤트를 걸어줌
+    btns.forEach((el)=>{
+        el.addEventListener("click", clickBtn);
+    });
+//처음 시작할때 가장 첫번째 img를 활성화시켜주는 함수
+    (function carouselInit() {
         carousel_Item[current].classList.add("active");
-    }
-    
-    function isMaxLangth(num){
-        if(num > carousel_Item.length-1){
+    })()
+// 다음 current가 총 길이보다 크거나 0보다 작은지 구분하여 조정해주는 함수
+    function isMaxLangth(num) {
+        if (num > carousel_Item.length - 1) {
             return num = 0;
         }
-        if(num < 0){
-            return num = carousel_Item.length-1;
+        if (num < 0) {
+            return num = carousel_Item.length - 1;
         }
         return num;
     }
-    
-    function nextState(event){
-        let currentStart = false;
-        let currentEnd = false;
-        for(let item of carousel_Item){
-            item.classList.remove("active");
-            item.classList.remove("prev-active");
-            item.classList.remove("next-active");
-            item.classList.remove("prev");
-            item.classList.remove("next");
+
+    function nextState(event, preCurrent) {
+        for (let item of carousel_Item) {
+            item.classList = "";
         }
-        if(current === carousel_Item.length-1){
-            currentEnd = true;
+        if (preCurrent == null) {
+            current = isMaxLangth(current);
+            next_img = isMaxLangth(current + 1);
+            pre_img = isMaxLangth(current - 1);
         } else {
-            currentEnd = false;
+            current = current;
+            next_img = preCurrent;
+            pre_img = preCurrent;
         }
-    
-        if(current === 0){
-            currentStart = true;
-        } else {
-            currentStart = false;
-        }
-    
-        current = isMaxLangth(current);
-        next_img = isMaxLangth(current+1);
-        pre_img = isMaxLangth(current-1);
-        if(event){
+
+        if (event) {
             carousel_Item[pre_img].classList.add("prev");
             carousel_Item[current].classList.add("prev-active");
         } else {
@@ -64,13 +82,46 @@ function carousel(car){
         }
     }
 
-    
-    
-    carouselInit();
+
+// 이미지의 수만큼 pagenation을 만들어주는 함수
+    function createPageNation() {
+        let pagenation = document.createElement("ul");
+        for (let i = 0; i < carousel_Item.length; i++) {
+            let li = document.createElement("li");
+            let button = document.createElement("button");
+            let isNext = true;
+            button.textContent = i
+
+            button.classList.add(`button`, `button${i}`);
+            button.addEventListener("click", () => {
+                let preCurrent = current;
+
+                if (current === i) {
+                    return;
+                }
+
+                if (i < current) {
+                    isNext = false;
+                } else {
+                    isNext = true;
+                }
+                current = i;
+                nextState(isNext, preCurrent);
+            });
+            li.appendChild(button);
+            pagenation.appendChild(li);
+        }
+        pagenation.classList.add("pagenation");
+        carousel_box.appendChild(pagenation);
+    }
+
+    if (car.pagenation) {
+        createPageNation();
+    }
+
 }
 let box = document.querySelector(".carousel-box");
 let Carousel = new carousel({
-    target : box,
-    pagenation : false,
+    target: box,
+    pagenation: true,
 });
-
